bae9f842c3a025db0d5ebf4302634123099e3b92
frontendprof <https://github.com/frontendprof>
2021-07-08 19:42:51 +0500

    controlled input config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from "react"
+import axios from "axios"
 import Cart from "./components/Cart"
 import Header from "./components/Header"
 import Drawer from "./components/Drawer"
@@ -10,13 +11,13 @@ function App() {
   const [items,setItems]=useState([])
   const [drawerItems,setDrawerItems]=useState([])
   const [openDrawer,setOpenDrawer]=useState(false);
+  const [searchInput,setSearchInput]=useState("")
 
   useEffect(()=>{
-    fetch("https://60e153115a5596001730f08d.mockapi.io/items")
+    axios.get("https://60e153115a5596001730f08d.mockapi.io/items")
     .then(res=>{
-      return res.json()
-    }).then(items=>{
-      setItems(items)
+      console.log(res.data)
+      setItems(res.data)
     })
   },[])
 
@@ -24,6 +25,9 @@ function App() {
     setDrawerItems(prev=>[...prev,obj])
   }
 
+  const searchHandler=e=>{
+    setSearchInput(e.target.value)
+  }
   return (
     <div className="wrapper clear">  
         {openDrawer&&<Drawer closeDrawer={()=>setOpenDrawer(false)} 
@@ -34,15 +38,19 @@ function App() {
 
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>{searchInput? `Поиск по запросу: ${searchInput}` : "Все кроссовки" }</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="search_icon" />
-            <input type="text" placeholder="Поиск ..." />
+            <input type="text" placeholder="Поиск ..." onChange={searchHandler} value={searchInput}/>
+            {searchInput&& <img className="clearBtn" src="/img/btn-remove.svg" alt="clear_icon" onClick={()=>setSearchInput("")}/>}
+            
           </div>
         </div>
         
         <div className="d-flex flex-wrap">
-            {items.map(i=>(
+            {items
+            .filter(item=>item.title.toLowerCase().includes(searchInput))
+            .map(i=>(
               <Cart title={i.title} price={i.price} imageUrl={i.imageUrl} key={i.title}
                 onPlus={(obj)=>onAddToDrawer(i)}
               />
