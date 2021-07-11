47e1638a5241df34fb277a078910fd0764ea9e82
frontendprof <https://github.com/frontendprof>
2021-07-11 17:53:26 +0500

    Conditional rendering at work

--- src/App.js
+++ src/App.js
@@ -15,14 +15,21 @@ function App() {
 
   useEffect(()=>{
     axios.get("https://60e153115a5596001730f08d.mockapi.io/items")
-    .then(res=>{
-      console.log(res.data)
-      setItems(res.data)
-    })
+    .then(res=>{ setItems(res.data) })
+
+    axios.get("https://60e153115a5596001730f08d.mockapi.io/cart")
+    .then(res=>{ setDrawerItems(res.data) })
   },[])
 
   const onAddToDrawer=(obj)=>{
+    axios.post("https://60e153115a5596001730f08d.mockapi.io/cart",obj);
     setDrawerItems(prev=>[...prev,obj])
+  }
+
+  const onRemoveFromDrawer=(id)=>{
+    console.log(id)
+    // axios.delete(`https://60e153115a5596001730f08d.mockapi.io/cart/${id}`);
+    setDrawerItems(prev=>prev.filter(item=>item.id!==id))
   }
 
   const searchHandler=e=>{
@@ -31,7 +38,7 @@ function App() {
   return (
     <div className="wrapper clear">  
         {openDrawer&&<Drawer closeDrawer={()=>setOpenDrawer(false)} 
-          items={drawerItems}
+          items={drawerItems} onRemove={onRemoveFromDrawer}
         />}           
         <Header openDrawer={()=>setOpenDrawer(true)}/>
 
@@ -52,7 +59,7 @@ function App() {
             .filter(item=>item.title.toLowerCase().includes(searchInput))
             .map(i=>(
               <Cart title={i.title} price={i.price} imageUrl={i.imageUrl} key={i.title}
-                onPlus={(obj)=>onAddToDrawer(i)}
+                onPlus={(obj)=>onAddToDrawer(i)} 
               />
             ))}
         </div>
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Drawer = ({closeDrawer,items}) => {
+const Drawer = ({closeDrawer,items,onRemove}) => {
     return (
         <div className="overlay">
             <div className="drawer">
@@ -8,25 +8,45 @@ const Drawer = ({closeDrawer,items}) => {
                 Корзина
                 <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="remove_icon" onClick={closeDrawer}/>
                 </h2>
+                {
+                    items.length>0?
+                        (<div className="items">
+                            {items.map(obj=>(
+                                <div className="cartItem d-flex align-center mb-20" key={obj.title}>
+                                    <div className="cartItemImg" style={{ backgroundImage:`url(${obj.imageUrl})` }}></div>
 
-                <div className="items">
+                                    <div className="mr-20 flex">
+                                    <p className="mb-5">{obj.title}</p>
+                                    <b>{obj.price} руб.</b>
+                                    </div>
+                                    <img 
+                                        onClick={()=>onRemove(obj.id)}
+                                        className="removeBtn" 
+                                        src="/img/btn-remove.svg" 
+                                        alt="remove_icon" 
+                                    />
+                                </div>
+                            ))}       
+                        </div>):
 
-                    {items.map(obj=>(
-                        <div className="cartItem d-flex align-center mb-20" key={obj.title}>
-                            <div className="cartItemImg" style={{ backgroundImage:`url(${obj.imageUrl})` }}></div>
+                        (<div className="cartEmpty d-flex align-center justify-center flex-column flex">
+                            <img 
+                                className="mb-20" style={{ width:"120px", height:"120px" }}
+                                src="/img/empty-cart.jpg" 
+                                alt="empty_cart" 
+                            />
+                            <h2>Корзина пустая</h2>
+                            <p className="opacity-6">Добавьте хотя бы одну пару кроссовок чтобы сделать заказ</p>
+                            <button className="greenButton">
+                                <img src="/img/arrow.svg" alt="arrow_icon" />
+                                Вернутся назад
+                            </button>
+                        </div>)
+                }
 
-                            <div className="mr-20 flex">
-                            <p className="mb-5">{obj.title}</p>
-                            <b>{obj.price} руб.</b>
-                            </div>
-                            <img className="removeBtn" src="/img/btn-remove.svg" alt="remove_icon" />
-                        </div>
-                    ))}
 
-                
 
-        
-                </div>
+
                 
                 <div className="cartTotalBlock">
                     <ul>
